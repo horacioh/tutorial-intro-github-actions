b59358b4b17e7e475df52a914a5543ba14e881c2
Horacio Herrera <hi@horacioh.com>
2019-11-26 11:40:33 +0100

    test fallido

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ test("array with no dupes should return same array", () => {
 });
 
 test("array with dupes should return smaller array", () => {
-  expect(removeDupes([1, 2, 3, 3, 3])).toStrictEqual([1, 2, 3]);
+  expect(removeDupes([1, 2, 3, 3, 3])).toStrictEqual([1, 2, 3, 1]);
 });
 
 test("array with no values should return empty array", () => {
